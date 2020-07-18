b937b08e835b884917d9fc7136888cc2a3de72c6
Jacob <jabcog@hotmail.com>
2020-07-19 09:23:42 +1200

    Overall game look and grass

---
+++ Assets/Scripts/GrassClump.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassClump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        foreach(HingeJoint h in GetComponentsInChildren<HingeJoint>())
+        {
+            Vector3 axis = h.axis;
+            axis.z = Random.Range(0, 360);
+            h.axis = axis;
+        }
+    }
+
+
+}
