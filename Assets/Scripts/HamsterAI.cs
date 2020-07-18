e2f657c68c1cfeba4de642ba9d4f4b929247ad72
Ben <ben_sloboda@hotmail.com>
2020-07-18 20:58:58 +1200

    Hamster State Engine Frameworked

--- Assets/Scripts/HamsterAI.cs
+++ Assets/Scripts/HamsterAI.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class HamsterAI : MonoBehaviour
 {
-    enum State{gototarget, lookfortarget};
+    enum State{lured, hungry, wrangled, bored, eating};
     private Rigidbody rb;  
-    public float restartDelay = 5f; 
     public float HamSpeed = 3f;
     public float HamBounce = 1f;
-    public Vector3 HamRotate;
-    private int randSeed = 0;
-    public bool isLured = false;
-    public float lookForTargetTimer = 5f; 
-    public float goToTargetTimer = 5f;  
+    private int randSeed = 0; 
     public Transform diamante;
     private State myState;
     public Vector3 lurePoint;
     private Vector3 smoothDampVelocityREF;
     private Transform lookTransform;
-    public float turnSpeed;
+    public float turnSpeed;    
+    
+    public float hungerTimer = 5f; 
+    public float luredTimer = 0f; 
+    public float wrangledTimer = 10f;
+    public float boredTimer = 8f;
+    public float eatingTimer = 4f;
     
     void Awake()
     {
         lookTransform = new GameObject().transform;
-        myState = State.gototarget;
+        myState = State.lured;
         rb = GetComponent<Rigidbody>();
-        HamRotate = new Vector3(0, 100, 0);
         lurePoint = LurePoint();
     }
 
@@ -43,27 +43,52 @@ public class HamsterAI : MonoBehaviour
 
     void Update()
     {
-
+// Manual poop
         if (Input.GetKeyDown(KeyCode.Space))
         {
             PoopDiamond();
         }
-
-        if (lookForTargetTimer == 0f && myState == State.gototarget)
+// State lured, but not dead (reset state)
+        if (hungerTimer == 0f && myState == State.lured)
         {
-            myState = State.lookfortarget;
-            goToTargetTimer = 5f;
+            myState = State.hungry;
+            luredTimer = 5f;
         }
-        if(goToTargetTimer == 0f && myState == State.lookfortarget)
+// State hungry, but not lured        
+        if (luredTimer == 0f && myState == State.hungry)
         {
             lurePoint = LurePoint();
-            myState = State.gototarget;
+            myState = State.lured;
             randSeed = Random.Range(1,4);
-            lookForTargetTimer = 5f;
+            hungerTimer = 5f;
         }
-        goToTargetTimer = Mathf.Max(0, goToTargetTimer - Time.deltaTime);
-        lookForTargetTimer = Mathf.Max(0, lookForTargetTimer - Time.deltaTime);
-        rb.MovePosition(transform.position + ((transform.forward * HamSpeed) + (transform.up * HamBounce)) * Time.deltaTime);
+// State bored, but not eating        
+        if (boredTimer == 0f && myState == State.bored)
+        {
+            myState = State.eating;
+            // turn off run animation
+            eatingTimer = 10f;
+        }
+// State wrangled, but not hungry        
+        if (wrangledTimer == 0f && myState == State.wrangled)
+        {
+            myState = State.hungry;
+            luredTimer = 10f;
+        }
+// State eating, but not bored        
+        if (eatingTimer == 0f && myState == State.eating)
+        {
+            PoopDiamond();
+            myState = State.bored;
+            boredTimer = 7f;   
+        }
+// This block counts down all the timers        
+        hungerTimer = Mathf.Max(0, hungerTimer - Time.deltaTime);
+        luredTimer = Mathf.Max(0, luredTimer - Time.deltaTime);
+        boredTimer = Mathf.Max(0, boredTimer - Time.deltaTime);
+        wrangledTimer = Mathf.Max(0, wrangledTimer - Time.deltaTime);
+        eatingTimer = Mathf.Max(0, eatingTimer - Time.deltaTime);
+        //rb.MovePosition(transform.position + ((transform.forward * HamSpeed) + (transform.up * HamBounce)) * Time.deltaTime);
 
         if (transform.position.y < -100)
         {
@@ -72,19 +97,22 @@ public class HamsterAI : MonoBehaviour
 
         switch (myState)
         {
-            case State.gototarget:
-                // pick target
-
-
-                rb.AddRelativeForce(Vector3.up * HamSpeed, ForceMode.Force);
-
-                // run to target
-                //rb.MovePosition(transform.position + ((transform.forward * HamSpeed) + (transform.up * HamBounce)) * Time.deltaTime);
+            case State.lured:
+                // run at lure
+                rb.MovePosition(transform.position + ((transform.forward * HamSpeed) + (transform.up * HamBounce)) * Time.deltaTime);
+//                rb.AddRelativeForce(Vector3.forward * HamSpeed, ForceMode.Force);
+                //rb.AddRelativeForce(Vector3.forward * HamBounce, ForceMode.Force);
                 break;
-            case State.lookfortarget:
+            case State.hungry:
+                // look around for lure
                 lookTransform.position = Vector3.SmoothDamp(lookTransform.position, lurePoint, ref smoothDampVelocityREF, turnSpeed);
                 transform.LookAt(lookTransform);
-                // rotate around
+                break;
+            case State.wrangled:
+                // stop moving
+                break;
+            case State.bored:
+                // look for grass
                 break;
             default:
                 break;
@@ -99,5 +127,12 @@ public class HamsterAI : MonoBehaviour
     void DestroyHamster()
     {
         Destroy(gameObject);
+    }
+    
+    public void Wrangled()
+    {
+        myState = State.wrangled;
+        // Will wait ten seconds and then remove parent and escape?
+        wrangledTimer = 10f;
     }
 }
